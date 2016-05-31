b7828e2c75e903ebac2cf96445b17ebbffe9d391
Cristi <joracristi@gmail.com>
2016-06-01 00:36:06 +0300

    Initial commit

---
+++ DataAcces/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DataAcces.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DataAcces.Context
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext() : base("DataContext", throwIfV1Schema: false)
+        {
+            
+        }
+
+        public static DataContext Create()
+        {
+            return new DataContext();
+        }
+    }
+}
